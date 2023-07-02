43ac2903c78714c092d4e0ea17287458df7e46b9
AlecUrbany <alecurbany@gmail.com>
2023-07-02 00:23:37 -0700

    fixed indents

--- DesignPattern/Visited.java
+++ DesignPattern/Visited.java
@@ -7,21 +7,21 @@ import Assignment2.Twitter.UserGroup;
 
 public class Visited implements Visitor
 {
-    @Override
-    public void visit(User user)
-    {
-        JOptionPane.showMessageDialog(null, "User Total: " + user.getUsers().size());
-    }
+	@Override
+	public void visit(User user)
+	{
+		JOptionPane.showMessageDialog(null, "User Total: " + user.getUsers().size());
+	}
 
-    @Override
-    public void print(String msg)
-    {
-        System.out.println(msg);
-    }
+	@Override
+	public void print(String msg)
+	{
+		System.out.println(msg);
+	}
 
-    @Override
-    public void visit(UserGroup group)
-    {
-        throw new UnsupportedOperationException("");
-    }
+	@Override
+	public void visit(UserGroup group)
+	{
+		throw new UnsupportedOperationException("");
+	}
 }
